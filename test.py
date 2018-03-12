da70c25ff568589e07c4d9da0361eeb77c6bc18f
haoivu <haoivu@hotmail.com>
2018-03-12 02:44:25 +0100

    dasf

---
+++ test.py
@@ -0,0 +1,33 @@
+import csv #imports module csv
+from pprint import pprint
+from collections import OrderedDict
+
+filea = "1.csv"
+fileb = "2.csv"
+output = "3.csv"
+
+delim = "\t" #set your own delimiter
+
+newFile = csv.reader(open(filea,"r"),delimiter=delim)
+next(newFile)
+oldFile = csv.reader(open(fileb,"r"),delimiter=delim)
+#open csv readers
+
+oldFileAsList = OrderedDict()
+newFileAsList = OrderedDict()
+
+# prepare changes from file B
+for row in oldFile:
+    for i in range (0 , 9):
+        oldFileAsList[i+1] = row[i]
+    print(oldFileAsList)
+
+# write new changed rows
+with open(output, "w") as fileoutput:
+    csvwriter = csv.writer(fileoutput, delimiter=delim)
+    for row in newFile:
+        # needs to check whether there are any changes prepared
+        if row[1] in oldFileAsList:
+            # change the item
+            row[3] = oldFileAsList[row[1]]
+        csvwriter.writerow(row)
